42b9fa23d87f3bc61f74d0897231284ef7de76f1
Christopher Wellons <mosquitopsu@gmail.com>
2009-12-13 00:48:26 -0500

    New cell type: Growth

---
+++ src/GrowthCell.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+import java.util.*;
+
+// This is the basic cell that implements Conway's Game of Life
+public class GrowthCell extends Cell {
+    private Vector<Cell> adj = null;
+    private Color[] colors = null;
+    private int curState, nextState;
+    private static Random rand = new Random();
+
+    public GrowthCell() {
+        this(rand.nextInt(4));
+    }
+
+    public GrowthCell(int state) {
+        curState = nextState = state;
+	adj = new Vector<Cell>();
+	colors = new Color[4];
+        colors[0] = new Color(255, 255, 255);
+        colors[1] = new Color(0, 255, 0);
+        colors[2] = new Color(0, 127, 255);
+        colors[3] = new Color(0, 0, 255);
+    }
+
+    // Adds adjacent cells for polling
+    public void addAdj(Vector<Cell> cells) {
+        adj.addAll(cells);
+    }
+
+    // Return current cell state
+    public int getState() {
+        return curState;
+    }
+
+    // Get the color corresponding to the current cell state
+    public Color getColor() {
+        return colors[curState];
+    }
+
+    // Poll adjacent cells and update the next state
+    public void update() {
+        int liveCount = 0;
+        Iterator<Cell> it = adj.iterator();
+        while (it.hasNext()) {
+            Cell cell = it.next();
+            if (cell.getState() > 0)
+                liveCount = liveCount + 1;
+        }
+
+        // Like the original, but if we remain alive, we grow
+        if (curState > 0 && (liveCount == 2 || liveCount == 3))
+            nextState = curState + 1;
+        else if (curState == 0 && liveCount == 3)
+            nextState = 1;
+        else
+            nextState = 0;
+
+        if (nextState > 3)
+            nextState = 3;
+    }
+
+    // Built-in factory: create a new cell with a random state
+    public Cell divide() {
+        return divide(rand.nextInt(2));
+    }
+
+    // Built-in factory: create a new cell with given state
+    public Cell divide(int state) {
+        return new GrowthCell(state);
+    }
+
+    // Advance cell to the previously calculated next state
+    public void sync() {
+        curState = nextState;
+    }
+}
